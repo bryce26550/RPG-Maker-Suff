85fa75284ee814bbb284819b2f2a6c6300038c6a
bryce26550 <129105196+bryce26550@users.noreply.github.com>
2025-02-20 14:52:21 -0500

    update 2/20/25 2:48
    Formbar plugin works now

---
+++ Test/js/plugins/Formplug.js
@@ -0,0 +1,121 @@
+/*:
+ * @target MZ
+ * @plugindesc Allows users to give a thumbs up, down or wiggle to Formbar and interact with Formbar API via socket.io-client.
+ * @author Bryce Lynd
+ * 
+ * @command thumbsUp
+ * @text Thumbs Up
+ * @desc Sends a ThumbsUp
+ * 
+ * @command thumbsDown
+ * @text Thumbs Down
+ * @desc Sends a ThumbsDown
+ * 
+ * @command wiggle
+ * @text Wiggle
+ * @desc Sends a Wiggle
+ * 
+ * @command opps
+ * @text Opps
+ * @desc Removes the user's vote
+ * 
+ * @help
+ * This plugin allows users to give a thumbs up, down or wiggle to Formbar.
+ * 
+ * Plugin Commands:
+ *  Formplug thumbsUp
+ *  Formplug thumbsDown
+ *  Formplug wiggle
+ *  Formplug opps
+ * 
+ */
+
+(() => {
+    const FORMBAR_URL = 'https://formbeta.yorktechapps.com/';
+    const API_KEY = '2846ed55b81943cb2b3dcd9a6b1bd31b1fb7d51fced5704fff558ae2da7de39af7a10707acb3f86abd4969e40eff099cee1a3840907370e62881ca8d227c06b9';
+    // Check if Socket.IO is already loaded
+    if (typeof io === "undefined") {
+        let script = document.createElement("script");
+        script.src = "https://cdn.socket.io/4.7.2/socket.io.min.js";
+        script.onload = function () {
+            console.log("Socket.IO loaded!");
+            startSocketConnection(); // Call function to initialize socket after loading
+        };
+        document.head.appendChild(script);
+    } else {
+        startSocketConnection();
+    }
+
+    function startSocketConnection() {
+        const socket = io(FORMBAR_URL, {
+            extraHeaders: {
+                api: API_KEY
+            }
+        }); // Replace with your server URL
+
+        socket.on('connect', () => {
+            console.log('Connected');
+            socket.emit('getActiveClass');
+        });
+
+        socket.on('setClass', (classId) => {
+            console.log(`The user is currently in the class with id ${classId}`);
+        });
+
+        socket.on('connect_error', (error) => {
+            if (error.message == 'xhr poll error') {
+                console.log('no connection');
+            } else {
+                console.log(error.message);
+            }
+
+            setTimeout(() => {
+                socket.connect();
+            }, 5000);
+        });
+
+        let classId = 0;
+        socket.on('setClass', (userClass) => {
+            classId = userClass;
+        });
+
+        socket.on('classEnded', () => {
+            socket.emit('leave', classId);
+            classId = '';
+            socket.emit('getUserClass', { api: API_KEY });
+        });
+
+        socket.on('joinRoom', (classCode) => {
+            if (classCode) {
+                socket.emit('vbUpdate');
+            }
+        });
+
+        socket.on('vbUpdate', (data) => {
+            console.log(data);
+        });
+
+        PluginManager.registerCommand("Formplug", "thumbsUp", () => {
+            console.log("ur mum");
+            
+            socket.emit("pollResp", "Up")
+        });
+        PluginManager.registerCommand("Formplug", "thumbsDown", () => {
+            console.log("stink = you");
+            
+
+            socket.emit('pollResp', "Down");
+        });
+        PluginManager.registerCommand("Formplug", "wiggle", () => {
+            console.log("not sure");
+            
+            socket.emit('pollResp', 'wiggle');
+        });
+        PluginManager.registerCommand("Formplug", "opps", () => {
+            console.log("made mistake");
+            
+            socket.emit('pollResp','opps');
+        });
+    }
+
+})();
